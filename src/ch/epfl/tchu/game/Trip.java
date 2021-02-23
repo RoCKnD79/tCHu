ed4d1a3de9401aeb33453f04b166e07ca5e3bf56
romch <roman.danylovych@epfl.ch>
2021-02-23 11:58:58 +0100

    progrès dans Trip

--- src/ch/epfl/tchu/game/Trip.java
+++ src/ch/epfl/tchu/game/Trip.java
@@ -1,15 +1,57 @@
 package ch.epfl.tchu.game;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class Trip {
 
-    public Trip(Station from, Station to, int points) {
+    private final Station from;
+    private final Station to;
+    private final int points;
 
+    public Trip(Station from, Station to, int points) throws NullPointerException, IllegalArgumentException {
+
+        this.from = Objects.requireNonNull(from);
+        this.to = Objects.requireNonNull(to);
+
+        if (points <= 0) {
+            throw new IllegalArgumentException("points should be > 0");
+        } else {
+            this.points = points;
+        }
     }
 
-    public static List<Trip> all(List<Station> from, List<Station> to, int points) {
-        return null;
+    public static List<Trip> all(List<Station> from, List<Station> to, int points)
+            throws IllegalArgumentException {
+
+        if (from == null || to == null || points <= 0) {
+            throw new IllegalArgumentException("List null or points <= 0");
+        }
+
+        List<Trip> list = new ArrayList<Trip>();
+        for(Station f : from) {
+            for(Station t : to) {
+                list.add(new Trip(f, t, points));
+            }
+        }
+        return list;
+    }
+
+    public Station from() { return from; }
+
+    public Station to() { return to; }
+
+    public int points() {
+        return points;
+    }
+
+    public int points(StationConnectivity connectivity) {
+        if (connectivity.connected(from, to) == true) {
+            return points;
+        } else {
+            return -points;
+        }
     }
 
 }
