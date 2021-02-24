9382a482588a355243c3b4a9bf31e4d3f677c2b0
Christopher <christopher.soriano@epfl.ch>
2021-02-24 21:16:50 +0100

    trucs que jai fait aujourdhui christo

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -9,9 +9,9 @@ public final class Ticket implements Comparable<Ticket>{
     private Station fromStation;
     private ArrayList<String> text = new ArrayList<>();
 
-    /*First constructor for ticket class, verifies that list of trips isn't null and that all trips have same origin.
+    /**First constructor for ticket class, verifies that list of trips isn't null and that all trips have same origin.
      @param List<Trip> trips
-    */
+    **/
     public Ticket(List<Trip> trips) throws IllegalArgumentException{
 
         if (trips == null){
@@ -27,33 +27,29 @@ public final class Ticket implements Comparable<Ticket>{
         }
         }
 
-    /*Second constructor for ticket class, creates a unique trip.
+    /**Second constructor for ticket class, creates a unique trip.
       @param Station from
       @param Station to
       @int points
-     */
+     **/
     public Ticket(Station from, Station to, int points){
         this(List.of());
         //this(List.of(trip = new Trip(from, to, points)))
 
     }
 
-    /*
+    /**
     returns number of points for the specific ticket
     @param StationConnectivity connectivity
-     */
+     **/
     public int points(StationConnectivity connectivity){
         return this.points(connectivity);
     }
 
     public String text(){
-
     }
 
     private String  computeText(List<Trip> trips){
-
-
-
     }
 
     /*
